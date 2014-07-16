e3683c0ec1f09076a6f8dae08aff3ed785743789
N-Manos <manos.nick@gmail.com>
2014-07-15 18:53:16 -0700

    E-mail validation
    Added e-mail validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
 	self.validates_presence_of :first_name
+	validates :email, :email => {:message => I18n.t('validations.errors.models.user.invalid_email')}, :mx => {:message => I18n.t('validations.errors.models.user.invalid_mx')}
 end
